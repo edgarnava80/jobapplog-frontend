5a74fe68fc50972fedac570adb8155a1cf16b901
edgarnava80 <edgarnr@hotmail.com>
2020-10-08 16:42:36 -0500

    Bundle with docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-const path = require("path")
+const path = require('path')
 
 module.exports = {
-  entry: "./app/index.js",
+  entry: './app/index.js',
   output: {
-    publicPath: "/",
-    path: path.resolve(__dirname, "app"),
-    filename: "bundled.js"
+    publicPath: '/',
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'bundled.js'
   },
-  mode: "development",
-  devtool: "source-map",
+  mode: 'development',
+  devtool: 'source-map',
   devServer: {
     port: 4020,
-    contentBase: path.join(__dirname, "app"),
+    contentBase: path.join(__dirname, 'app'),
     hot: true,
-    historyApiFallback: { index: "index.html" }
+    historyApiFallback: { index: 'index.html' }
   },
   module: {
     rules: [
@@ -21,9 +21,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-react", ["@babel/preset-env", { targets: { node: "12" } }]]
+            presets: ['@babel/preset-react', ['@babel/preset-env', { targets: { node: '12' } }]]
           }
         }
       }
