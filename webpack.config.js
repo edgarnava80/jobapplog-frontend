b720baef24f28144a433f1d3ae1f1607b6c6048d
edgarnava80 <edgarnr@hotmail.com>
2020-10-09 20:35:19 -0500

    Production files redy in docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
+const currenTask = process.env.npm_lifecycle_event
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
+const config = {
   entry: './app/index.js',
   output: {
-    publicPath: '/',
+    //publicPath: '/',
     path: path.resolve(__dirname, 'docs'),
     filename: 'bundled.js'
   },
+  plugins: [new HtmlWebpackPlugin({ template: './app/index.html' })],
   mode: 'development',
   devtool: 'source-map',
   devServer: {
     port: 4020,
-    contentBase: path.join(__dirname, 'app'),
-    hot: true,
-    historyApiFallback: { index: 'index.html' }
+    //contentBase: path.join(__dirname, 'app'),
+    contentBase: path.resolve(__dirname, 'docs'),
+    hot: true
+    //historyApiFallback: { index: 'index.html' }
   },
   module: {
     rules: [
@@ -23,10 +27,17 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-react', ['@babel/preset-env', { targets: { node: '12' } }]]
+            //presets: ['@babel/preset-react', ['@babel/preset-env', { targets: { node: '12' } }]]
+            presets: ['@babel/preset-react', '@babel/preset-env']
           }
         }
       }
     ]
   }
 }
+
+if (currenTask == 'build') {
+  config.mode = 'production'
+}
+
+module.exports = config
